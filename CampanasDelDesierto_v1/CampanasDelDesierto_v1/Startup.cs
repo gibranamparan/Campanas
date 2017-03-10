c4f3a4215aa600277f4ec1c608854e385e12b5b0
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-03-09 18:57:27 -0700

    Initial Commit

---
+++ CampanasDelDesierto_v1/CampanasDelDesierto_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CampanasDelDesierto_v1.Startup))]
+namespace CampanasDelDesierto_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
