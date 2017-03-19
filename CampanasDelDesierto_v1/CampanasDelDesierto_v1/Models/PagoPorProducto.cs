c1936945b3c8e59b7d6d91e3de26ab9a49d51715
IbrahimLopez <cotulopez@gmail.com>
2017-03-19 15:27:09 -0700

    Cambio en valor

--- CampanasDelDesierto_v1/CampanasDelDesierto_v1/Models/PagoPorProducto.cs
+++ CampanasDelDesierto_v1/CampanasDelDesierto_v1/Models/PagoPorProducto.cs
@@ -11,9 +11,9 @@ namespace CampanasDelDesierto_v1.Models
         public double cantidadProducto { get; set; }
         public int numeroSemana { get; set; }
         public string cheque { get; set; }
-        public int abonoAnticipo { get; set; }
+        public int? abonoAnticipo { get; set; }
         public string tipoProducto { get; set; }
-        public int garantiaLimpieza { get; set; }
+        public int? garantiaLimpieza { get; set; }
         //public int porcentajeEjidal { get; set; }
 
 
