8a7019f9a41b893d4dd45a2654af9aeb4a1ccaf9
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-04-05 18:31:26 -0700

    Listado de productores como vista por defecto

--- CampanasDelDesierto_v1/CampanasDelDesierto_v1/App_Start/RouteConfig.cs
+++ CampanasDelDesierto_v1/CampanasDelDesierto_v1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CampanasDelDesierto_v1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Productores", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
