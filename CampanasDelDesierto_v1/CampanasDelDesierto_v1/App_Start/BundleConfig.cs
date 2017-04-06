25f37364b4f099e3838e1e0c809d77ef5fae711b
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-04-06 07:46:46 -0700

    Correccion de estilos de layout

--- CampanasDelDesierto_v1/CampanasDelDesierto_v1/App_Start/BundleConfig.cs
+++ CampanasDelDesierto_v1/CampanasDelDesierto_v1/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace CampanasDelDesierto_v1
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
